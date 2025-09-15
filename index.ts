cba9d5554fed21f194dc176a4c0d1448b9cb21da
ouafcode <ouafaesaim@gmail.com>
2025-09-15 20:42:44 +0100

    generat tools

--- index.ts
+++ index.ts
@@ -1,10 +1,25 @@
-import {generateText} from "ai";
-import {google} from "@ai-sdk/google"
+import { stepCountIs, streamText } from "ai";
+import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getFileChangesInDirectoryTool } from "./tools";
 
-const {text} = await generateText({
+const codeReviewAgent = async (prompt: string) => {
+  const result = streamText({
     model: google("models/gemini-2.5-flash"),
-    prompt:"What is an AI agent?"
+    prompt,
+    system: SYSTEM_PROMPT,
+    tools: {
+      getFileChangesInDirectoryTool: getFileChangesInDirectoryTool,
+    },
+    stopWhen: stepCountIs(10),
+  });
 
-})
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+};
 
-console.log(text);
+// Specify which directory the code review agent should review changes in your prompt
+await codeReviewAgent(
+  "Review the code changes in 'C:/Users/info/Desktop/my_agent' directory, make your reviews and suggestions file by file",
+);
